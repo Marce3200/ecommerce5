1194de2db47e9b19b575a196cdf65c9ec2de9b68
antoniaarga <maargandonaa@gmail.com>
2023-01-22 16:05:21 -0300

    base carrito

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,10 +1,116 @@
-import React from 'react'
-
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "../Cart/cart.css";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import mouse1 from "../img/mouse1.jpg";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <Container>
+      <center>
+        <Card
+          className="card-carrito d-flex flex-column"
+          style={{ width: "75%" }}
+        >
+          <Card.Body>
+            <Row className="align-items-center">
+              <Col lg={3}>
+                <img className="foto-carrito" src={mouse1} />
+              </Col>
 
-export default Cart
+              <Col lg={2}>
+                <p>Kone XP Air</p>
+              </Col>
+
+              <Col lg={2}>
+                <input
+                  id="number"
+                  type="number"
+                  value="42"
+                  className="w-50 input-number"
+                />
+              </Col>
+
+              <Col lg={2}>
+                <p className="texto-mini">$45.990</p>
+              </Col>
+              <Col lg={2}>
+                <p className="texto-mini">X</p>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+
+        <Card
+          className="card-carrito d-flex flex-column"
+          style={{ width: "75%" }}
+        >
+          <Card.Body>
+            <Row className="align-items-center">
+              <Col lg={3}>
+                <img className="foto-carrito" src={mouse1} />
+              </Col>
+
+              <Col lg={2}>
+                <p>Kone XP Air</p>
+              </Col>
+
+              <Col lg={2}>
+                <input
+                  id="number"
+                  type="number"
+                  value="42"
+                  className="w-50 input-number"
+                />
+              </Col>
+
+              <Col lg={2}>
+                <p className="texto-mini">$45.990</p>
+              </Col>
+              <Col lg={2}>
+                <p className="texto-mini">X</p>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+
+        <Card
+          className="card-carrito d-flex flex-column"
+          style={{ width: "75%" }}
+        >
+          <Card.Body>
+            <Row className="align-items-center">
+              <Col lg={3}>
+                <img className="foto-carrito" src={mouse1} />
+              </Col>
+
+              <Col lg={2}>
+                <p>Kone XP Air</p>
+              </Col>
+
+              <Col lg={2}>
+                <input
+                  id="number"
+                  type="number"
+                  value="42"
+                  className="w-50 input-number"
+                />
+              </Col>
+
+              <Col lg={2}>
+                <p className="texto-mini">$45.990</p>
+              </Col>
+              <Col lg={2}>
+                <p className="texto-mini">X</p>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </center>
+    </Container>
+  );
+};
+
+export default Cart;
